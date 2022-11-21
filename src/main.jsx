6ffe05dd35ec472791b055cb4beeffca41f601a3
Lukmon492 <lukmon492@gmail.com>
2022-11-21 22:59:23 +0100

    directories and files modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+
+import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
