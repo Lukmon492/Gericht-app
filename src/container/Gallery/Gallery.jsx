0b958cd545017004950da8ed7425bc99d755174d
Lukmon492 <lukmon492@gmail.com>
2022-11-22 23:46:18 +0100

    another commit

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsInstagram } from "react-icons/bs";
+import { BsArrowLeftShort } from "react-icons/bs";
 import { SubHeading } from "../../components";
 import { images } from "../../constants";
 import "./Gallery.css";
