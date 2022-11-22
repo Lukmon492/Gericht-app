d06028e3a352230e0ef45484de8d533caa5b8ec5
Lukmon492 <lukmon492@gmail.com>
2022-11-22 06:03:57 +0100

    stylesheet added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,23 @@ import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
 import "./Navbar.css";
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => (
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
